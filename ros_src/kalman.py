9ecfee00c38f06e62d7878c6569a7eb2e1918ab6
godhj93 <godhj@unist.ac.kr>
2021-06-17 21:13:40 +0900

    correct kalman filter allocating problem

--- ros_src/kalman.py
+++ ros_src/kalman.py
@@ -35,9 +35,10 @@ class MultiTargetTracker:
 		# error covariance and other things
 		self.Q = 1.0*np.eye(4)
 
+		R = 50
 		self.R = np.matrix(
-			[[50,0],
-			[0,50]])
+			[[R,0],
+			[0,R]])
 
 		self.P = 100*np.eye(4)
 
@@ -66,7 +67,7 @@ class MultiTargetTracker:
 		for ID,idx in enumerate(range(len(data.results))):
 
 			self.object_state.append(
-			  np.array([
+			  [
 			data.results[idx].x_center, #0
 			data.results[idx].y_center, #1
 			data.results[idx].w, # 2
@@ -79,41 +80,42 @@ class MultiTargetTracker:
 			] ,
 			
 
-			))
+			)
 
 
-	def kalman_filter(self,object_states):
+	def kalman_filter(self,prev_state,object_state):
+	
+		# Get estimated position with gaussian noise
+		
+		prev_x = prev_state[0]
+		prev_y = prev_state[1]
 
-		self.kalman_states = []
-		for object_state in object_states:
-			# Get estimated position with gaussian noise
-			
-			pos_x = np.array(object_state[0],dtype=np.float32).item()
-			pos_y = np.array(object_state[1],dtype=np.float32).item()
-			label = object_state[-1]
+		self.x[0] = prev_x
+		self.x[2] = prev_y
+		
 
-			self.x_pos_estimated = pos_x
-			self.y_pos_estimated = pos_y
-			
-			# Kalman filter 
+		pos_x = np.array(object_state[0],dtype=np.float32).item()
+		pos_y = np.array(object_state[1],dtype=np.float32).item()
+		label = object_state[-1]
 
-			self.x_prediction = self.A * self.x
+		self.x_pos_estimated = pos_x + np.random.normal(0,30)
+		self.y_pos_estimated = pos_y + np.random.normal(0,30)
+  		
+		# Kalman filter 
 
-			self.P_prediction = self.A * self.P * np.transpose(self.A) + self.Q
+		self.x_prediction = self.A * self.x
 
-			self.K = self.P_prediction * np.transpose(self.H) * np.linalg.inv(self.H * self.P_prediction * np.transpose(self.H) + self.R)
+		self.P_prediction = self.A * self.P * np.transpose(self.A) + self.Q
 
-			self.Z = np.transpose(np.matrix([self.x_pos_estimated, self.y_pos_estimated]))
+		self.K = self.P_prediction * np.transpose(self.H) * np.linalg.inv(self.H * self.P_prediction * np.transpose(self.H) + self.R)
 
-			self.x = self.x_prediction + self.K * (self.Z - self.H * self.x_prediction)
+		self.Z = np.transpose(np.matrix([self.x_pos_estimated, self.y_pos_estimated]))
 
-			self.P = self.P_prediction - self.K * self.H * self.P_prediction
+		self.x = self.x_prediction + self.K * (self.Z - self.H * self.x_prediction)
 
+		self.P = self.P_prediction - self.K * self.H * self.P_prediction
 
-
-			self.kalman_states.append((self.x[0],self.x[2],label))
-
-		return np.array(self.kalman_states)
+		return [self.x[0].item(),self.x[2].item(),label]
 		
 			
 		
@@ -135,10 +137,14 @@ class MultiTargetTracker:
 				distance_list.append(l2_distance)
 
 
+			try:
+				current_id = np.argmin(distance_list)
+				current[current_id][-1] = prev[prev_id][-1]
+			except:
+				rospy.logerr("No datalist")
 
-			current_id = np.argmin(distance_list)
 
-			current[current_id][-1] = prev[prev_id][-1]
+			
 
 
 
@@ -152,7 +158,7 @@ class MultiTargetTracker:
 			#current[current_id][-1] = prev[prev_id][-1]
 
 
-		return distance_list
+		
 			
 
 
@@ -161,28 +167,40 @@ class MultiTargetTracker:
 	def image(self,data):
 		
 		self.cv_rgb_image = self.bridge.imgmsg_to_cv2(data,'bgr8')
-		for states in self.object_state:
-			x_min = np.array(states[4],dtype=np.int32)
-			y_min = np.array(states[6],dtype=np.int32)
+		for states in zip(self.object_state):
+			# x_min = np.array(states[4],dtype=np.int32)
+			# y_min = np.array(states[6],dtype=np.int32)
 
-			#for value error
-
-			x_center = np.array(states[0], dtype=np.float32)
-			y_center = np.array(states[1], dtype=np.float32)
-
+			# #for value error
+			
+			x_center = np.array(states[0][0], dtype=np.float32)
+			y_center = np.array(states[0][1], dtype=np.float32)
 			x_center = np.array(x_center, dtype=np.int32)
 			y_center = np.array(y_center, dtype=np.int32)
-			label = states[8]
-		
-			color = (0,150,255)#(np.random.randint(255),np.random.randint(255),np.random.randint(255))
 
-			#self.cv_rgb_image = cv2.circle(self.cv_rgb_image, (x_center,y_center), 5, color, -1)
+			x_center_estimated = np.array(states[0][0], dtype=np.float32)
+			y_center_estimated = np.array(states[0][1], dtype=np.float32)
+			x_center_estimated = np.array(states[0][0], dtype=np.int32)
+			y_center_estimated = np.array(states[0][1], dtype=np.int32)
+			
+			label = states[0][8]
+		
+			color_red = (0,0,255)#(np.random.randint(255),np.random.randint(255),np.random.randint(255))
+			color_green = (0,255,0)
+			self.cv_rgb_image = cv2.circle(self.cv_rgb_image, (x_center,y_center), 5, color_red, -1)
+			self.cv_rgb_image = cv2.circle(self.cv_rgb_image, (x_center_estimated,y_center_estimated), 3, color_green, -1)
 			cv2.putText(
 				self.cv_rgb_image,
 				label, 
-				(x_min,y_min-10),
-				 cv2.FONT_HERSHEY_SIMPLEX, 1, color, 2, cv2.LINE_AA)
+				(x_center,y_center-10),
+				 cv2.FONT_HERSHEY_SIMPLEX, 1, color_red, 2, cv2.LINE_AA)
 
+			# cv2.putText(
+			# 	self.cv_rgb_image,
+			# 	label, 
+			# 	(x_min,y_min-10),
+			# 	 cv2.FONT_HERSHEY_SIMPLEX, 1, color_red, 2, cv2.LINE_AA)
+			pass
 		cv2.imshow('window',self.cv_rgb_image)
 
 		cv2.waitKey(3)
@@ -198,7 +216,7 @@ class MultiTargetTracker:
 if __name__ =='__main__':
 	
 	MTT = MultiTargetTracker()
-	rate = rospy.Rate(100)
+	rate = rospy.Rate(1000)
 
 	prev_objects = MTT.object_state # x,y,label
 
@@ -208,29 +226,32 @@ if __name__ =='__main__':
 	
 		current_objects = MTT.object_state		
 
-		kalman_states = MTT.kalman_filter(current_objects)
+		kalman_prediction_list = []
+		for item_prev, item_current in zip(prev_objects,current_objects):
+			
+			kalman_prediction_list.append(MTT.kalman_filter(item_prev,item_current))
 
-
-		for item_kalman , item_current in zip(kalman_states, current_objects):
-
-			# print("==")
-			# print(item_current)
-			
-			
-			item_current[0] = copy.deepcopy(item_kalman[0].item())
-			item_current[1] = copy.deepcopy(item_kalman[1].item())
-
-			
-			
-			
-
-			
-		
 		
 
-		l2_distances = MTT.Euclidean(prev_objects,current_objects)
+
+		#kalman_states = MTT.kalman_filter(current_objects[0])
+
+		#print(kalman_states)
+		# for item_kalman , item_current in zip(kalman_states, current_objects):
+
+		# 	print(item_kalman)
+			
+			
+			
+		# 	item_current[0] = copy.deepcopy(item_kalman[0].item())
+		# 	item_current[1] = copy.deepcopy(item_kalman[1].item())
+
+
+		MTT.Euclidean(prev_objects,current_objects)
 		
-		
+		# for item in current_objects:
+			
+		# 	print(item[-1])
 		
 		
 		prev_objects = current_objects
